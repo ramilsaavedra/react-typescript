f91d85b17342edfd6bffb2353ad28015e6b18ea7
Ramil Saavedra <ramilsmcl@gmail.com>
2022-06-11 18:55:46 +0800

    ts in class component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,19 @@ import { createRoot } from 'react-dom/client';
 // import Parent from './props/Parent';
 // import GuestList from './state/GuestList';
 // import UserSearch from './state/UserSearch';
-import EventComponent from './events/EventComponent';
+// import EventComponent from './events/EventComponent';
+import UserSearch from './classes/UserSearch';
 
 const App = () => {
-  return <EventComponent />;
+  return (
+    <UserSearch
+      users={[
+        { name: 'Jet', age: 27 },
+        { name: 'AJ', age: 27 },
+        { name: 'Ramil', age: 27 },
+      ]}
+    />
+  );
 };
 
 createRoot(document.querySelector('#root')!).render(<App />);
