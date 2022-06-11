de6ac91ae932f4b1a0a9042fee5e6c476b569d4e
Ramil Saavedra <ramilsmcl@gmail.com>
2022-06-11 18:46:12 +0800

    event handlers

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 // import Parent from './props/Parent';
 // import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
 
 const App = () => {
-  return <UserSearch />;
+  return <EventComponent />;
 };
 
 createRoot(document.querySelector('#root')!).render(<App />);
