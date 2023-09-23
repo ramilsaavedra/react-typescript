b82242033e01079ae75859f4ce155cdc0963ee4d
Ramil Saavedra <ramilsmcl@gmail.com>
2023-09-23 18:46:10 +0800

    type annotation for events

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -2,9 +2,16 @@ const EventComponent: React.FC = () => {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e);
   };
+
+  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log('im being dragged', e);
+  };
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me
+      </div>
     </div>
   );
 };
