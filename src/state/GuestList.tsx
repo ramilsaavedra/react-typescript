612f9ef0b75fe83037a68eb2d696d92cc70fbf51
Ramil Saavedra <ramilsmcl@gmail.com>
2023-09-23 17:44:09 +0800

    type for state array

---
+++ src/state/GuestList.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const GuestList: React.FC = () => {
+  const [name, setName] = useState('');
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const addGuest = () => {
+    setGuests([...guests, name]);
+    setName('');
+  };
+
+  return (
+    <div>
+      <h3>Guest List</h3>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={addGuest}>Add Guest</button>
+    </div>
+  );
+};
+
+export default GuestList;
